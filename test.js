e118e542fc08a742dea3547cf1a4e9fd78df3f33
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-02-04 20:00:07 +0900

    add more tests to cover various cases

--- test.js
+++ test.js
@@ -64,46 +64,58 @@ var includeNothing = [
   '}'
 ].join('\n');
 
-test('filterDeclarations()', function(t) {
-  t.plan(6);
+test('postcssFilterDeclarations()', function(t) {
+  t.plan(8);
 
-  t.equal(
-    postcss().use(filterDeclarations({props: 'display'})).process(fixture).css, includeDisplay,
-    'should select the rules based on the property name.'
-  );
+  t.equal(filterDeclarations.name, 'postcssFilterDeclarations', 'should have a function name.');
 
   t.equal(
     postcss().use(filterDeclarations({properties: 'display'})).process(fixture).css,
     includeDisplay,
-    'should use `properties` option as an alias of `props` option.'
+    'should select the rules based on the property name.'
+  );
+
+  t.equal(
+    postcss().use(filterDeclarations({props: 'display'})).process(fixture).css,
+    includeDisplay,
+    'should use `props` option as an alias of `properties` option.'
   );
 
   t.equal(
     postcss().use(filterDeclarations({props: ['border', 'content']})).process(fixture).css,
     includeBorderAndContent,
-    'should select the rules based on an array of the property names.'
+    'should select the rules in response to the array of property names.'
   );
 
-  t.equal(
-    postcss().use(filterDeclarations({
-      props: 'display',
-      exclude: true
-    })).process(fixture).css,
-    includeBorderAndContent,
-    'should exclude the rules based on the property name, using `exclude` option.'
-  );
+  var options = {
+    props: ['border', 'content'],
+    exclude: true
+  };
 
   t.equal(
-    postcss().use(filterDeclarations({
+    postcss().use(filterDeclarations(options)).process(fixture).css,
+    includeDisplay,
+    'should exclude the rules in response to the property names, using `exclude` option.'
+  );
+
+  t.deepEqual(
+    options,
+    {
       props: ['border', 'content'],
       exclude: true
-    })).process(fixture).css,
-    includeDisplay,
-    'should exclude the rules based on an array of the property names, using `exclude` option.'
+    },
+    'should not modify the original option object.'
   );
 
   t.equal(
-    postcss().use(filterDeclarations()).process(fixture).css, includeNothing,
+    postcss().use(filterDeclarations({properties: [{foo: 'bar'}, 123]})).process(fixture).css,
+    includeNothing,
+    'should ignore non-string values in the `properties` option.'
+  );
+
+  t.equal(
+    postcss().use(filterDeclarations()).process(fixture).css,
+    includeNothing,
     'should exclude all properties when it takes no arguments.'
   );
 });
